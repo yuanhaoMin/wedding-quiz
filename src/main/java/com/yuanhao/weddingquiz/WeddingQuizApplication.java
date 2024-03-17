2c5144f9e134616878fcf9985d1b6179b068a009
Yuanhao <min.yuanhao.1997.11.12@outlook.com>
2024-03-17 16:32:28 +0100

    initial setup

---
+++ src/main/java/com/yuanhao/weddingquiz/WeddingQuizApplication.java
@@ -0,0 +1,13 @@
+package com.yuanhao.weddingquiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeddingQuizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeddingQuizApplication.class, args);
+	}
+
+}
